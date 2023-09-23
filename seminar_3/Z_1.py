2d675087f2b944eed1564607b78506b5f0c50c2d
Anastasia Lazareva <laza-nastya@yandex.ru>
2023-09-23 21:59:33 +0300

    add Z_1

---
+++ seminar_3/Z_1.py
@@ -0,0 +1,12 @@
+# Дан список чисел. Определите, сколько в нем встречается различных чисел.
+# input: [1, 1, 2, 0, -1, 3, 4, 4]
+# output: 6
+
+
+myList = []
+for i in range(5):
+    myList.append(input('введите число: '))
+
+mySet = set(myList)
+print(*mySet)
+print(len(mySet))
